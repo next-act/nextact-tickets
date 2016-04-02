49816748f4cd622908caa7d9af0e3b1b666f89a2
Budmonde Duinkharjav <budmonde@gmail.com>
2016-04-01 20:36:09 -0400

    admin privileges created

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,14 +2,14 @@
 if (Count.find().count() === 0 ) {
 	Count.insert({
 		show: 'Thursday April 7th',
-		availableSeats: 125
+		availableSeats: 164
 	});
 	Count.insert({
 		show: 'Friday April 8th',
-		availableSeats: 125
+		availableSeats: 164
 	});
 	Count.insert({
 		show: 'Saturday April 9th',
-		availableSeats: 125
+		availableSeats: 164
 	});
 }
